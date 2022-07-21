4332ec65f00dcb749d8fe72b2ffdc61ab525fe78
Alejandra MCuitino <ale.morales11@gmail.com>
2022-07-21 23:15:11 +0200

    DetailsJob style card

--- src/components/JobDetail/InfoDetailJob.jsx
+++ src/components/JobDetail/InfoDetailJob.jsx
@@ -1,36 +1,38 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "../../styles/infoDetailJob.css"
 
 
-function InfoDetailJob ({ id, jobName, companyName, hsPublished, jobLocation, onSiteOrRemote, description, datePublished, jobTitle, companyLink, companyLogo}) 
-{
-    return(
+function InfoDetailJob({ id, jobName, companyName, hsPublished, jobLocation, onSiteOrRemote, description, datePublished, jobTitle, companyLink, companyLogo }) {
+    return (
         <>
-         <div className="row g-0">
-             <div className="col-md-4 d-flex align-items-center">  
-                 <img src={companyLogo} className="img-fluid rounded-start" alt="Logo Empresa"></img>
-                 <ul>
-                     <li className="card-rightText">{jobTitle}</li>
-                     <li className="card-rightText">{onSiteOrRemote}</li>
-                     <li className="card-rightText">{jobLocation}</li>
-                     <li className="card-rightText">{datePublished}</li>
-                     </ul>
-            </div>
-            <div className="col-md-8">
-                <div className="card-body">
-                    <ul>
-                        <li><h5 className="card-title">{jobName}</h5></li>
-                        <a href= {companyLink}>
-                        <li className="card-text">{companyName}</li>
-                        </a>
-                        <li>"Publicado "{hsPublished}</li>
-                        <li>{description}</li>
+            <div className="cardDetail">
+                <div className="col-md-4 d-flex">
+                    <img src={companyLogo} className="img-fluid rounded-start rounded pl" alt="Logo Empresa"></img>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <ul className="iconsList1">
+                                <li><h5 className="card-title">{jobName}</h5></li>
+                                <a href={companyLink}>
+                                    <li className="card-text">{companyName}</li></a>
+                                <li className="card-publish">"Publicado "{hsPublished}</li>
+                                <li>{description}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <ul className="iconsList">
+                        <li className="card-rightText"><i id="iconsCards" class="bi bi-plus-square-fill"></i>{jobTitle}</li>
+                        <li className="card-rightText"><i id="iconsCards" className="bi bi-house-fill"></i>{onSiteOrRemote}</li>
+                        <li className="card-rightText"><i id="iconsCards" className="bi bi-geo-alt-fill"></i>{jobLocation}</li>
+                        <li className="card-rightText"><i id="iconsCards" className="bi bi-megaphone-fill"></i>{datePublished}</li>
                     </ul>
                 </div>
-            </div>
-        </div>
-        </>
-     ) 
- }
 
- export default InfoDetailJob
+            </div>
+        </>
+    )
+}
+
+export default InfoDetailJob
